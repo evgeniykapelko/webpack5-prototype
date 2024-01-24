9addf40f1cd41c7e24e9533f6f2dfddb28b24cef
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-01-24 02:54:29 +0200

    Create webpack configuration: Part 2

--- build/267.ecf69764bc96c323d8f5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebpack_5_ski_prototype=self.webpackChunkwebpack_5_ski_prototype||[]).push([[267],{267:(e,t,p)=>{p.r(t),p.d(t,{default:()=>k});var c=p(294);const k=function(){return c.createElement("div",null," ABOUT ")}}}]);
